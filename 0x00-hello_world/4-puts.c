68394ba857a5245dfbc7e33661b2aabb576b4a33
Richmond Abenney <richmondabenney@gmail.com>
2023-03-17 10:09:31 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main -  entry point
-*return: always 0 (Success)
+* main - prints a line puts function
+* Return: 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
