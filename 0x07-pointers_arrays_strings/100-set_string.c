4febe18b5738a375e55c9280c647053526886f7f
Richmond Abenney <richmondabenney@gmail.com>
2023-04-03 14:24:32 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Check the code string
+ * @s: input
+ * @to: input to string
+ * Return: 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
