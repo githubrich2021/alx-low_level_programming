eeea5bc32be2b6b0a698952e89568de6faadc547
Richmond Abenney <richmondabenney@gmail.com>
2023-04-05 21:59:18 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,7 +11,7 @@ int _pow_recursion(int x, int y)
 {
 	if (x < 0)
 		return (-1);
-	if (x ==  0 || y == -1)
+	if (y ==  0 || x == -1)
 		return (1);
-	return (x * _pow_recursion(y, x - 1));
+	return (y * _pow_recursion(x, y - 1));
 }
