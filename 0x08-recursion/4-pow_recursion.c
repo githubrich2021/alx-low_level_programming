3e43bddd6234d5c457f00d26477457eb60bf9df1
Richmond Abenney <richmondabenney@gmail.com>
2023-04-05 21:52:40 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry piont
+ * @x: print x
+ * @y: print y
+ *
+ * Return: result = power(x, y)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x < 0)
+		return (-1);
+	if (y ==  0 || x == -1)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
