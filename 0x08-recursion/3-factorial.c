23acf4e5498403fcd708378b9c7d1048146ba784
Richmond Abenney <richmondabenney@gmail.com>
2023-04-05 20:49:58 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ * @n: inpurt factorial
+ *
+ * Return: factorial number
+ */
+int factorial(int n)
+{
+	/**
+	 * -1 if n < 0
+	 */
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
