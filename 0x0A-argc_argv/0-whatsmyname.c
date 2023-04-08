d9fb21cc0415ea0f23e2bda116b7a9847fe5f420
Richmond Abenney <richmondabenney@gmail.com>
2023-04-08 15:04:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point name
+ * @argc: count
+ * @argv: vector
+ *
+ * Retrun: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
