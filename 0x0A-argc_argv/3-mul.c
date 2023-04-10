1003b255fd37011abd04ae58cd7b558461ebaad3
Richmond Abenney <richmondabenney@gmail.com>
2023-04-10 06:27:59 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,19 @@
  * main - Multipling integres
  * @argc: output
  * @argv: arguments
- *
  * Return: 0 if is true, 1 if is false
  */
 int main(int argc, char *argv[])
 {
 	int r, n;
+
 	if (argc == 3)
-	{
+{
 		r = atoi(argv[1]);
 		n = atoi(argv[2]);
 			printf("%d\n", r * n);
 			return (0);
-	}
+}
 	printf("Error\n");
 	return (1);
 }
