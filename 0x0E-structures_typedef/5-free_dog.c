df17e4be8bbfa4e250b259e37f9583b3c04b1744
Richmond Abenney <richmondabenney@gmail.com>
2023-04-17 06:49:24 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "doh.h"
+#include <stdlib.h>
+
+
+/**
+* free_dog - Entry point
+* @d: struct dog to free
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
