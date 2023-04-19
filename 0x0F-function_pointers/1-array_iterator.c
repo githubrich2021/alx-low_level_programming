5ce27ad36f390c31280a0fe0807cc059e726e261
Richmond Abenney <richmondabenney@gmail.com>
2023-04-19 05:02:46 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry point
+ * @array: array
+ * @size: size to print
+ * @action: hex pointer
+ * Retrun: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int l;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; l < size; l++)
+	{
+		action(array[l]);
+	}
+}
