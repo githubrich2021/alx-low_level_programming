5396fe0f46adeb4b23a8b3e1bf97f71c1325ea22
Richmond Abenney <richmondabenney@gmail.com>
2023-05-01 19:27:29 +0000

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* reverse_listint - Entry point
+* @head: printing node
+*
+* Return: printing new list node
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+
+	return (*head);
+}
